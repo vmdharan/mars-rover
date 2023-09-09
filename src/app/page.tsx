7f3e19f17148d63d1bea66098eb75219e815bc6b
vmdharan <vmdharan@users.noreply.github.com>
2023-09-09 12:52:32 +1000

    Set up Next.js environment
    Set up Next.js environment.

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Mars Rover images&nbsp;
+        </p>
+
+      </div>
+    </main>
+  )
+}
